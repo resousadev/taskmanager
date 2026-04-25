f8b304a1d3fce7c476d46ddb3eeb8a3fe0db4143
renanrcs <renan.cavalcante@yahoo.com>
2026-04-25 11:32:05 -0300

    refactor CreateTaskUseCaseTest to remove unused Spring context dependency

--- src/test/java/dio/taskmanager/application/CreateTaskUseCaseTest.java
+++ src/test/java/dio/taskmanager/application/CreateTaskUseCaseTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
