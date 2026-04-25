5aee791cce1adfa55017313dfc2e6d236cd8e572
renanrcs <renan.cavalcante@yahoo.com>
2026-04-25 11:31:32 -0300

    refactor CreateTaskUseCaseTest to use mocked TaskRepository

--- src/test/java/dio/taskmanager/application/CreateTaskUseCaseTest.java
+++ src/test/java/dio/taskmanager/application/CreateTaskUseCaseTest.java
@@ -1,26 +1,38 @@
 package dio.taskmanager.application;
 
 import dio.taskmanager.application.input.CreatTaskInput;
-import dio.taskmanager.insfrastructure.repository.InMemoryTaskRepository;
-import org.junit.jupiter.api.BeforeEach;
+import dio.taskmanager.domain.Task;
+import dio.taskmanager.domain.TaskRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CreateTaskUseCaseTest {
 
-    CreateTaskUseCase useCase;
+    @Mock
+    TaskRepository taskRepository;
 
-    @BeforeEach
-    void setUp() {
-        this.useCase = new CreateTaskUseCase(new InMemoryTaskRepository());
-    }
+    @InjectMocks
+    CreateTaskUseCase useCase;
 
     @Test
     void should_create_task_successufully() {
         // Given
         var input = new CreatTaskInput("Test Task", java.util.Optional.of("This is a test task"));
+
+        when(taskRepository.save(any(Task.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
 
         // When
         var output = useCase.execute(input);
@@ -30,5 +42,7 @@ class CreateTaskUseCaseTest {
         assertNotNull(output.id());
         assertEquals("Test Task", output.title());
         assertEquals(java.util.Optional.of("This is a test task"), output.description());
+
+        verify(taskRepository, times(1)).save(any(Task.class));
     }
 }
