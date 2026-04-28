1598b7fe21fd0d8852c92c74dc57f4a50a237e48
resousadev <resousadev@gmail.com>
2026-04-27 21:51:10 -0300

    add GlobalExceptionHandler and implement task management endpoints in TaskController

---
+++ src/main/java/dio/taskmanager/insfrastructure/http/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package dio.taskmanager.insfrastructure.http;
+
+import dio.taskmanager.domain.TaskNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return ResponseEntity.status(404).body(ex.getMessage());
+    }
+
+}
