192532e46dd6910c24e283891c8ac239457a900a
resousadev <resousadev@gmail.com>
2026-04-27 22:28:46 -0300

    refactor GlobalExceptionHandler to streamline validation error handling

--- src/main/java/dio/taskmanager/insfrastructure/http/GlobalExceptionHandler.java
+++ src/main/java/dio/taskmanager/insfrastructure/http/GlobalExceptionHandler.java
@@ -3,13 +3,11 @@ package dio.taskmanager.insfrastructure.http;
 import dio.taskmanager.domain.TaskNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -17,23 +15,21 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+    public ResponseEntity<String> handleTaskNotFoundException(final TaskNotFoundException ex) {
         return ResponseEntity.status(404).body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationException(final MethodArgumentNotValidException ex) {
 
-        Map<String, String> errors = new HashMap<>();
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> error.getField(),
+                        error -> error.getDefaultMessage(),
+                        (existing, replacement) -> existing
+                ));
 
-        ex.getBindingResult().getAllErrors()
-                .forEach(error -> {
-                    String fieldName = ((FieldError) error).getField();
-                    String errorMessage = error.getDefaultMessage();
-                    errors.put(fieldName, errorMessage);
-                });
-        
         return errors;
     }
 
